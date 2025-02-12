5e194fc0d55c93fe23bd8a1348a829de179da946
GuiiHtto <guilherme.competi@gmail.com>
2025-02-12 16:19:02 -0300

    A 10 Variaveis de tipos de dados

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('Olá, Javascript!');
+
+let userName = 'Guilherme Henrique'
+
+document.getElementById('userName').innerHTML = userName;
+
+// let name = 'Master Yoda';
+// let age =  900;
+// let isJedi = true;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isJedi);
