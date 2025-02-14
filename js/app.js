61f35bec9de4259db026c0992872a9746bb6b285
GuiiHtto <guilherme.competi@gmail.com>
2025-02-14 14:55:39 -0300

    Add alt attributes for character images and improve accessibility

--- js/app.js
+++ js/app.js
@@ -1,59 +1,59 @@
-
-
 const LIST = [
     {
         id: 1,
         name: 'Master Yoda',
-        avatar: 'images/yoda.png'
-
+        avatar: 'images/yoda.png',
+        alt: 'Lego figure of Master Yoda with green skin and a lightsaber'
     },
     
     {
         id: 2,
         name: 'Darth Vader',
-        avatar: 'images/vader.png'
-
+        avatar: 'images/vader.png',
+        alt: 'Lego figure of Darth Vader with black armor and a red lightsaber'
     },
 
     {
         id: 3,
         name: 'Luke Skywalker',
-        avatar: 'images/luke.png'
-
+        avatar: 'images/luke.png',
+        alt: 'Lego figure of Luke Skywalker with a lightsaber'
     },
 
     {
         id: 4,
         name: 'Princess Leia',
-        avatar: 'images/leia.png'
-
+        avatar: 'images/leia.png',
+        alt: 'Lego figure of Princess Leia with white dress and buns hairstyle'
     },
 
     {
         id: 5,
         name: 'Han Solo',
-        avatar: 'images/hansolo.png'
-
+        avatar: 'images/hansolo.png',
+        alt: 'Lego figure of Han Solo with a blaster'
     },
 
     {
         id: 6,
         name: 'Chewbacca',
-        avatar: 'images/chewbacca.png'
+        avatar: 'images/chewbacca.png',
+        alt: 'Lego figure of Chewbacca with a bowcaster'
     },
     
     {
         id: 7,
         name: 'R2D2',
-        avatar: 'images/r2d2.png'
+        avatar: 'images/r2d2.png',
+        alt: 'Lego figure of R2D2, a blue and white astromech droid'
     },
 
     {
         id: 8,
         name: 'C3PO',
-        avatar: 'images/c3po.png'
+        avatar: 'images/c3po.png',
+        alt: 'Lego figure of C3PO, a golden protocol droid'
     }
-
 ]
 
 const App = new Vue({
@@ -73,9 +73,9 @@ const App = new Vue({
 
            const result = list.filter(item => {
                 return item.id !== id
-       })
+           })
 
-         this.characters = result
+           this.characters = result
        },
        search(){
 
@@ -88,13 +88,13 @@ const App = new Vue({
             const list = this.characters = LIST
 
             const result = list.filter(item => {
-            return item.name === this.searchName
+                return item.name === this.searchName
             })
 
             if(result.length <= 0){
                 alert('Character not found')
             } else {
-            this.characters = result 
+                this.characters = result 
             }
         }
     }
